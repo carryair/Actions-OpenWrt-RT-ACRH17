281632d8d9eb0319e5dba0b7b351b79716dfb898
sunrisesunsuper <64893511+sunrisesunsuper@users.noreply.github.com>
2020-07-14 18:13:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,6 +19,11 @@ sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_genera
 #修改默认 SSID为RT-ACRH17
 sed -i 's/OpenWrt/RT-ACRH17/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
+# Modify default wifi 速率 MU-MIMO
+#修改默认 
+#sed -i '/a set wireless.default_radio${devidx}.legacy_rates=0' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i '/a set wireless.default_radio${devidx}.mu_beamformer=1' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
 #取消bootstrap为默认主题
 #sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 #删除默认密码
